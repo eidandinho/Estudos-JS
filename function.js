2e1a7b7f50dfae291a95d5c73eda4afa2568e86e
Evandro Lima <evalveslima@hotmail.com>
2024-03-05 02:19:41 -0300

    Primeira Aula

---
+++ function.js
@@ -0,0 +1,23 @@
+function nome() {}
+nome();
+
+// -----------------
+var x = 1;
+function soma() {
+    x = x + 1;
+}
+
+console.log(x);
+soma();
+console.log(x);
+console.log(soma);
+// -----------------
+
+function divisao( x, y) {
+    return x / y;
+}
+
+console.log(divisao(7, 2));
+// -----------------
+
+//
